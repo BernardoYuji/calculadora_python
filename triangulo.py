593c601c979cd23fb14cdf0376a0f0c93c28e654
BernardoYuji <hanashirobernardo@gmail.com>
2025-08-20 19:05:47 -0300

    triangle area

---
+++ triangulo.py
@@ -0,0 +1,9 @@
+#Area de um triangulo#
+
+def area_triangulo():
+    b = int(input("Digte o valor de B: "))
+    h = int(input("Digite o valor de H: "))
+    
+    a = (b * h) / 2
+    print(f'O resultado da área do triangulo é {a}')
+area_triangulo()
